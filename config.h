3b270434cef9b5d1604ca5c5a1916beda0359a52
Rajdeepbar <rajdeep.flamestorm@gmail.com>
2019-05-17 11:38:02 +0530

    first commit

---
+++ config.h
@@ -0,0 +1,9 @@
+//config file for quadcopter
+//created on : 17/05/2019
+//@uthor : Rajdeep
+
+//-----------------motor pins-----------------------
+#define motor0 2
+#define motor1 3
+#define motor2 4
+#define motor3 5
