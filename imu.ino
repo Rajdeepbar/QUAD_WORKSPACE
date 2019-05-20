631725ceeb35e85f58da475ade872c19b8f8593d
Rajdeepbar <rajdeep.flamestorm@gmail.com>
2019-05-20 15:40:01 +0530

    imu.ino updated

--- imu.ino
+++ imu.ino
@@ -2,9 +2,12 @@
 
 //---------------MPU6050----------------------------------------
 int mpu_addr = 0x68; //with AD0 pulled down
-uint16_t mpu_pwr_1 = 0x6B; //power management 1
+uint16_t mpu_pwr_1 = 0x6B; //register address of PWR_MGMT_1
+uint16_t accel_start = 0x3B; //register address of ACCEL_XOUT_H
+uint16_t gyro_start =0x43; //register address of GYRO_XOUT_H
 
-//intiate i2c as master
+uint16_t Ax,Ay,Az,Gx,Gy,Gz;
+
 void mpu_init()
 {
     Wire.begin();
@@ -15,5 +18,17 @@ void mpu_init()
 }
 
 
+void mpu_get_data(){
+    Wire.beginTransmission(mpu_addr);
+    Wire.write(accel_start);
+    Wire.requestFrom(mpu_addr,6);
+    Ax=(Wire.read()<<8)|wireRead();
+    Ay=(Wire.read()<<8)|wireRead();
+    Az=(Wire.read()<<8)|wireRead();
 
-
+    Wire.write(gyro_start);
+    Wire.requestFrom(mpu_addr,6);
+    Gx=(Wire.read()<<8)|wireRead();
+    Gy=(Wire.read()<<8)|wireRead();
+    Gz=(Wire.read()<<8)|wireRead();
+}
--- motor_control.ino
+++ motor_control.ino
@@ -6,14 +6,15 @@ int m1=1000;
 int m2=1000;
 int m3=1000;
 
-    //create servo objects
-    Servo motor0;    
-    Servo motor1;    
-    Servo motor2;    
-    Servo motor3;
+//--------------create servo objects----------------
+Servo motor0;    
+Servo motor1;    
+Servo motor2;    
+Servo motor3;
+
+
 
 void motor_init(){
-
     //attach pins
     motor0.attach(motor_pin_0);
     motor1.attach(motor_pin_1);
@@ -33,4 +34,11 @@ void motor_run(int s0,int s1,int s2,int s3){
     motor1.writeMicroseconds(s1);
     motor2.writeMicroseconds(s2);
     motor3.writeMicroseconds(s3);
+}
+
+void motor_stop(){
+    motor0.writeMicroseconds(1000);
+    motor1.writeMicroseconds(1000);
+    motor2.writeMicroseconds(1000);
+    motor3.writeMicroseconds(1000);
 }
